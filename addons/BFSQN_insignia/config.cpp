6ac95e965dd074f040f6cefca49cd4f0b5735e1c
Connor Walsh <connor.w.misc@gmail.com>
2026-01-16 14:17:58 -0500

    Move insignia back in

---
+++ addons/BFSQN_insignia/config.cpp
@@ -0,0 +1,121 @@
+#include "script_component.hpp"
+
+class CfgPatches
+{
+    class ADDON
+    {
+        name = SPOKE;
+        author = AUTHOR;
+        requiredVersion = REQUIRED_VERSION;
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = { "BFSQN_core" };
+    };
+};
+
+class CfgUnitInsignia
+{
+    class bfsqn_insignia_member
+    {
+        displayName = "BFSQN | Member";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\general_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_aaRifleman
+    {
+        displayName = "BFSQN | AA Rifleman";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\aaRifleman_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_atRifleman
+    {
+        displayName = "BFSQN | AT Rifleman";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\atRifleman_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_breacher
+    {
+        displayName = "BFSQN | Breacher";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\breacher_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_eod
+    {
+        displayName = "BFSQN | Explosives Specialist";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\eod_insignia.paa";
+        scope = 2;
+    };
+    
+    class bfsqn_insignia_grenadier
+    {
+        displayName = "BFSQN | Grenadier";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\grenadier_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_heliPilot
+    {
+        displayName = "BFSQN | Helicopter Pilot";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\heliPilot_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_machinegunner
+    {
+        displayName = "BFSQN | Machinegunner";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\machinegunner_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_marksman
+    {
+        displayName = "BFSQN | Marksman";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\marksman_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_medic
+    {
+        displayName = "BFSQN | Medic";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\medic_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_planePilot
+    {
+        displayName = "BFSQN | Plane Pilot";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\planePilot_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_squadLead
+    {
+        displayName = "BFSQN | Squad Lead";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\squadLead_insignia.paa";
+        scope = 2;
+    };
+
+    class bfsqn_insignia_uavOperator
+    {
+        displayName = "BFSQN | UAV Operator";
+        author = "Blackforce Squadron";
+        texture = "\x\bfsqn_hub\addons\bfsqn_insignia\images\uavOperator_insignia.paa";
+        scope = 2;
+    };
+};
